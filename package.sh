b17acec81a2638d1a7bdddc6f7d748644b180462
Ha.Minh <minhhh@minhhuyha.info>
2017-12-18 14:19:06 +0700

    Add build folder

--- package.sh
+++ package.sh
@@ -9,7 +9,6 @@ rm -Rf $FRAMEWORK/$BUILD
 xcodebuild archive -project $FRAMEWORK/$FRAMEWORK.xcodeproj -scheme $FRAMEWORK -sdk iphoneos SYMROOT=$BUILD
 xcodebuild build -project $FRAMEWORK/$FRAMEWORK.xcodeproj -target $FRAMEWORK -sdk iphonesimulator SYMROOT=$BUILD
 
-cp -RL $FRAMEWORK/$BUILD/Release-iphoneos $FRAMEWORK/$BUILD/Release-universal
-# cp -RL $FRAMEWORK/$BUILD/Release-iphonesimulator/$FRAMEWORK_PATH/Modules/$FRAMEWORK.swiftmodule/* $FRAMEWORK/$BUILD/Release-universal/$FRAMEWORK_PATH/Modules/$FRAMEWORK.swiftmodule
-#
-lipo -create $FRAMEWORK/$BUILD/Release-iphoneos/$FRAMEWORK_PATH $FRAMEWORK/$BUILD/Release-iphonesimulator/$FRAMEWORK_PATH -output $FRAMEWORK/$BUILD/Release-universal/$FRAMEWORK_PATH
+cp -RL $FRAMEWORK/$BUILD/Release-iphoneos $BUILD
+
+lipo -create $FRAMEWORK/$BUILD/Release-iphoneos/$FRAMEWORK_PATH $FRAMEWORK/$BUILD/Release-iphonesimulator/$FRAMEWORK_PATH -output $BUILD/$FRAMEWORK_PATH
